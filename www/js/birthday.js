760450f309459784bf81d817481a02f016d48a5c
lentebloem <hi_yoo@yeah.net>
2015-01-31 05:56:10 -0800

    add sorting and updating table with angular.js

--- www/js/birthday.js
+++ www/js/birthday.js
@@ -21,8 +21,8 @@ function init() {
    // Attach "onsubmit" handler
    document.getElementById("submit").onclick = function(){
       createBD(
-         document.getElementById("lastname"),
          document.getElementById("firstname"),
+         document.getElementById("lastname"),
          document.getElementById("month"),
          document.getElementById("date"),
          document.getElementById("year")
@@ -45,19 +45,7 @@ function init() {
  *   inputID+"Error" if it exists; otherwise via an alert().
  */
 
-var bdlist = [{firstName: "First Name", lastName: "Last Name", month: "Month", date: "Date", 
-			year: "Year"}];
-function createBD(fname, lname, mm, dd, yyyy){
-	// if (mm.length != 2 || date.length != 2 || yyyy.length != 4) {
-	// 	console.log("the input should be in correct form!");
-	// 	return false;
-	// }
-	validateForm();
-	console.log("hey");
-		console.log(fname);
-		bdlist.add({firstName: fname, lastName: lname, month: mm, date: dd, year: yyyy});
-}
-
+//Form validation
 function validateForm() {
    return (isNotEmpty("lastname", "Please enter the last name!")
    		&& isNotEmpty("firstname", "Please enter the first name!")
@@ -146,56 +134,7 @@ function isLengthMinMax(inputId, errorMsg, minLength, maxLength) {
    showMessage(isValid, inputElement, errorMsg, errorElement);
    return isValid;
 }
- 
-// Return true if the input value is a valid email address
-// (For illustration only. Should use regexe in production.)
-function isValidEmail(inputId, errorMsg) {
-   var inputElement = document.getElementById(inputId);
-   var errorElement = document.getElementById(inputId + "Error");
-   var inputValue = inputElement.value;
-   var atPos = inputValue.indexOf("@");
-   var dotPos = inputValue.lastIndexOf(".");
-   var isValid = (atPos > 0) && (dotPos > atPos + 1) && (inputValue.length > dotPos + 2);
-   showMessage(isValid, inputElement, errorMsg, errorElement);
-   return isValid;
-}
- 
-// Return true if selection is made (not default of "") in <select> input
-function isSelected(inputId, errorMsg) {
-   var inputElement = document.getElementById(inputId);
-   var errorElement = document.getElementById(inputId + "Error");
-   var inputValue = inputElement.value;
-   // You need to set the default value of <select>'s <option> to "".
-   var isValid = inputValue !== "";
-   showMessage(isValid, inputElement, errorMsg, errorElement);
-   return isValid;
-}
- 
-// Return true if the one of the checkboxes or radio buttons is checked
-function isChecked(inputName, errorMsg) {
-   var inputElements = document.getElementsByName(inputName);
-   var errorElement = document.getElementById(inputName + "Error");
-   var isChecked = false;
-   for (var i = 0; i < inputElements.length; i++) {
-      if (inputElements[i].checked) {
-         isChecked = true;  // found one element checked
-         break;
-      }
-   }
-   showMessage(isChecked, null, errorMsg, errorElement);
-   return isChecked;
-}
- 
-// Verify password. The password is kept in element with id "pwId".
-// The verified password in id "verifiedPwId".
-function verifyPassword(pwId, verifiedPwId, errorMsg) {
-   var pwElement = document.getElementById(pwId);
-   var verifiedPwElement = document.getElementById(verifiedPwId);
-   var errorElement = document.getElementById(verifiedPwId + "Error");
-   var isTheSame = (pwElement.value === verifiedPwElement.value);
-   showMessage(isTheSame, verifiedPwElement, errorMsg, errorElement);
-   return isTheSame;
-}
+
  
 // The "onclick" handler for the "reset" button to clear the display
 function clearDisplay() {
@@ -211,3 +150,62 @@ function clearDisplay() {
    // Set initial focus
    document.getElementById("firstname").focus();
 }
+
+
+
+// var options = {
+// valueNames: [ "First Name",  "Last Name", "Month", "Date", "Year"]};
+
+var values = [];
+//var bdlist = new List('users', options, values);
+
+
+function createBD(fname, lname, mm, dd, yyyy){
+   // if (mm.length != 2 || date.length != 2 || yyyy.length != 4) {
+   //    console.log("the input should be in correct form!");
+   //    return false;
+   // }
+   validateForm();
+   values.push({fn: fname, ln: lname, m: mm, d: dd, y: yyyy});
+   updateTable();
+}
+
+function updateTable(){
+   var scope = this;
+   
+
+}
+
+//Table Sorting
+function SortableTableCtrl() {
+    var scope = this;
+    scope.body = values;
+    // data
+    scope.head = {
+        fn: "First Name",
+        ln: "Last Name",
+        m: "Month",
+        d: "Date",
+        y: "Year"
+    };
+    
+    scope.sort = {
+        column: 'b',
+        descending: false
+    };
+
+    scope.selectedCls = function(column) {
+        return column == scope.sort.column && 'sort-' + scope.sort.descending;
+    };
+    
+    scope.changeSorting = function(column) {
+        var sort = scope.sort;
+        if (sort.column == column) {
+            sort.descending = !sort.descending;
+        } else {
+            sort.column = column;
+            sort.descending = false;
+        }
+    };
+}
+
